8dc6e720b1a8a5e8bf45a24e5e7d7c4a82535dc6
GitTodd24 <todd.asante@gmail.com>
2025-01-12 12:13:55 -0600

    React Portfolio Update

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Resume from './pages/resume'
+import Portfolio from './pages/portfolio'
+import About from './pages/about'
+import Contact from './pages/contact'
+import NavBar from './components/navBar' 
+
+
+function App() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+   if (currentPage === 'About') {
+   return <About />;
+   }
+  if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+   }
+   return <Contact />;
+  }; 
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+
+
+  return (
+    <>
+      <NavBar handlePageChange={handlePageChange} currentPage={currentPage}/>
+    {renderPage()}
+    </>
+  )
+}
+
+export default App
